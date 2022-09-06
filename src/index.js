fd44927b6cddf813e9c918ecf3a470b1adfba56c
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-09-06 21:11:04 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// On app load, get all tasks from localStorage
+window.onload = loadTasks;
+
+// load tasks from localStorage
+function loadTasks () { }
+
+// add a task
+function addTask() {}
+
+// complete a task
+function completeTask() {}
+
+// remove a task
+function removeTask() {}
+
+//
+function getCurrentTask() {}
+
+// edit a task and update local storage
+function editTask() {}
