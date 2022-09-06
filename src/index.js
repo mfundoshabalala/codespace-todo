ff4ddef48fe8e6dbf5e43bf57220d59b51f71d18
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-09-06 21:32:33 +0200

    add and load tasks method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 // On app load, get all tasks from localStorage
 window.onload = loadTasks;
 
+const form = document.querySelector("form");
+// On form submit add task
+form && form.addEventListener("submit", (event) => {
+			event.preventDefault();
+			addTask();
+		});
+
 // load tasks from localStorage
-function loadTasks () { }
+function loadTasks () {
+	if (localStorage.getItem("tasks") === null) return;
+
+	let tasks = Array.from(JSON.parse(localStorage.getItem("tasks") || "[]"));
+
+	console.log("localStorage", tasks);
+}
 
 // add a task
-function addTask() {}
+function addTask () {
+	const task = document.querySelector("form input");
+
+	// return if task is empty
+	if (task?.value === "") {
+		alert("Task is empty!");
+		return false;
+	}
+
+	// check if the task already exist
+
+
+	// add task to localStorage
+	localStorage.setItem("tasks", JSON.stringify([
+		...JSON.parse(localStorage.getItem("tasks") || "[]"),
+		{
+			id: Date.now(),
+			task: task?.value,
+			completed: false,
+			date: Date.now()
+		}
+	]));
+}
 
 // complete a task
 function completeTask() {}
