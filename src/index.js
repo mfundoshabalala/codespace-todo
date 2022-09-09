34471be6b8254039b36c4216b676ae38ef482ea0
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-09-09 20:22:18 +0200

    ui updates

--- src/index.js
+++ src/index.js
@@ -110,13 +110,13 @@ function editTask (event) {
 		return;
 	}
 	// task already exist
-	tasks.forEach((task) => {
-		if (task.task === event.value) {
-			alert("Task already exist!");
-			event.value = currentTask;
-			return;
-		}
-	});
+	// tasks.forEach((task) => {
+	// 	if (task.task === event.value) {
+	// 		// alert("Task already exist!");
+	// 		event.value = currentTask;
+	// 		return;
+	// 	}
+	// });
 	// update task
 	tasks.forEach((task) => {
 		if (task.task === currentTask) {
