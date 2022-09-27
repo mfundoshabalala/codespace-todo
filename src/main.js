bda79487d3d6d3d8dc3297feae96bc62b429815c
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-09-27 16:54:41 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,29 @@
 // @ts-nocheck
 let form = document.getElementById("form");
-let input = document.getElementById("input");
+let tasks = document.getElementById("todos");
+//
 let titleInput = document.getElementById("titleInput");
 let dateInput = document.getElementById("dateInput");
 let descrInput = document.getElementById("descrInput");
+let statusInput = document.getElementById("statusInput");
 let msg = document.getElementById("msg");
-let tasks = document.getElementById("todos");
+//
+let btnAllTasks = document.getElementById("allTasks");
+let btnCompleteTasks = document.getElementById("completeTasks");
+let btnPendingTasks = document.getElementById("pendingTasks");
 
+// instantiate the data object
 let data = {};
+// instantiate the rendered tasks array
+let renderedTasks = []
+// instantiate the UI state
 let state = {
 	completed: false,
 	pending: false,
 }
 
 // accepts data on submit
-let acceptData = () => {
+const acceptData = () => {
 	data.push({
 		text: titleInput.value,
 		date: dateInput.value,
@@ -27,16 +36,16 @@ let acceptData = () => {
 };
 
 // reset form fields on submit
-let resetForm = () => {
+const resetForm = () => {
 	titleInput.value = "";
 	dateInput.value = "";
 	descrInput.value = "";
 }
 
 // create a new todo task
-let createTasks = () => {
+const createTasks = () => {
 	tasks.innerHTML = "";
-	data.map((x, y) => {
+	renderedTasks.map((x, y) => {
 		return (tasks.innerHTML += `
 			<div id=${ y }>
 				<span class="fw-bold">${ x.text }</span>
@@ -44,78 +53,122 @@ let createTasks = () => {
 				<p>${ x.description }</p>
 
 				<span class="options">
-					<i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+					<i onClick= "editTask(this)" class="fas fa-edit"></i>
 					<i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
-					<input type="checkbox" />
+					<input id="completeInput" type="checkbox" onClick ="completeTask(this)" />
 				</span>
 			</div>
     	`);
 	});
+
+	console.log(data);
 
 	resetForm();
 };
 
 // load tasks from local storage on reload
 (() => {
-  	data = JSON.parse(localStorage.getItem("data")) || [];
-	const tasks = createTasks();
+	data = JSON.parse(localStorage.getItem("data")) || [];
+	state = JSON.parse(localStorage.getItem("data")) || {};
 
 	// render all tasks
 	if (!state.completed && !state.pending) {
-		return tasks;
+		renderedTasks = data;
 	}
 
 	// render completed tasks
 	if (state.completed && !state.pending) {
-		return tasks.filter((task) => {
-			return task.complete === true;
-		});
+		// filter for tasks with a completed status of true
+		renderedTasks = data.filter(task => task.complete === true);
 	}
 
 	// render pending tasks
 	if (!state.completed && state.pending) {
-		return tasks.filter((task) => {
-			return task.complete === false;
-		});
+		// filter for tasks with a completed status of false
+		renderedTasks = data.filter(task => task.complete === false);
 	}
-
-	console.log(data);
+	//
+	createTasks();
 })();
 
 // delete todo task
-let deleteTask = (e) => {
+const deleteTask = (e) => {
 	e.parentElement.parentElement.remove();
 	data.splice(e.parentElement.parentElement.id, 1);
 	localStorage.setItem("data", JSON.stringify(data));
 };
 
 // edit todo task
-let editTask = (e) => {
+const editTask = (e) => {
 	let selectedTask = e.parentElement.parentElement;
-
+	//TODO: select a guard clause for completed tasks
+	// pre-populate the input(s) with previous values
 	titleInput.value = selectedTask.children[0].innerHTML;
 	dateInput.value = selectedTask.children[1].innerHTML;
 	descrInput.value = selectedTask.children[2].innerHTML;
-
+	// remove the task from local storage
 	deleteTask(e);
 };
+
+// complete todo task
+const completeTask = (e) => {}
 
 // submit event listener for the form
 form?.addEventListener("submit", (event) => {
 	event.preventDefault();
-	console.log("button clicked");
 
 	formValidation();
 });
 
 // validate the form fields on submit
-let formValidation = () => {
-	if (input?.value === "") {
+const formValidation = () => {
+	// every task should have a title at the very least
+	if (titleInput?.value === "") {
 		document.getElementById("msg").innerText = "Task cannot be blank";
-		console.log("failure");
 	} else {
 		document.getElementById("msg").innerText = "";
-		console.log("successs");
 		acceptData();
 	}
 };
+
+// render completed tasks
+const renderCompletedTasks = () => {
+	state.completed = true;
+	state.pending = false;
+
+	localStorage.setItem("state", JSON.stringify(state));
+}
+
+btnCompleteTasks?.addEventListener("click", (event) => {
+	event.preventDefault();
+	renderCompletedTasks();
+	// location.reload();
+});
+
+// render pending tasks
+const renderPendingTasks = () => {
+	state.pending = true;
+	state.completed = false;
+
+	localStorage.setItem("state", JSON.stringify(state));
+}
+
+btnPendingTasks?.addEventListener("click", (event) => {
+	event.preventDefault();
+	renderPendingTasks();
+	// location.reload();
+});
+
+// render all tasks
+const renderAllTasks = () => {
+	state.pending = false;
+	state.completed = false;
+
+	localStorage.setItem("state", JSON.stringify(state));
+};
+
+btnAllTasks?.addEventListener("click", (event) => {
+	event.preventDefault();
+	renderAllTasks();
+	// location.reload();
+});
